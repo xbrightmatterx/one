625dc2c9080fa62cf40346102bd1a5d36535e23b
Brian Graham <graham.briank@gmail.com>
2015-10-17 11:03:07 -0700

    upload requirements.txt

--- project/server.py
+++ project/server.py
@@ -1,11 +1,20 @@
 from flask import Flask, render_template
- 
+import webbrowser
+
 app = Flask(__name__)      
 
 @app.route('/')
 def home():
   return render_template('index.html')
 
+@app.route('/tweetsfeed')
+def hey():
+  Rurl = "http://www.mashable.com"
+  webbrowser.open(Rurl)
+  # return render_template('index.html')
+
+# if __name__ == "__main__":
+#   app.run(debug=True)
 #not sure if we need this anymore...
 #@app.route('/<path:path>')
 #def seeStaticFile(path):
